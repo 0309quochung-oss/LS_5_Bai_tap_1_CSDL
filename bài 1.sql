d518434f8c4ff2da939e812c4921645418031ef5
0309quochung-oss <0309quochung@gmail.com>
2026-05-05 20:00:16 +0700

    Update bài 1.sql

--- bài 1.sql
+++ bài 1.sql
@@ -1,3 +1,10 @@
-UPDATE PRODUCTS
-SET OriginalPrice = OriginalPrice * 0.9
-WHERE Category = 'Electronics';
+-- Code đúng:
+SELECT restaurant_name, address, rating
+FROM restaurants
+WHERE district IN ('Quận 1', 'Quận 3') AND rating > 4.0;
+
+-- lý do sai nằm ở dòng điều kiện tìm rating > 4.0 
+-- vì trong code gốc đã viết " WHERE district = 'Quận 1' OR district = 'Quận 3' AND rating > 4.0; " 
+-- thì sai ở logic OR và AND vì độ ưu tiên của AND lớn hơn OR 
+-- nên khi này đoạn code của chúng ta sẽ thành là " WHERE district = 'Quận 1' OR (district = 'Quận 3' AND rating > 4.0;) " 
+-- nên là em sài IN để gom hai cái lại thành tương đương cùng với nhau để hai cái đều được so sánh với AND rating > 4.0.
